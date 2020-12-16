3fa935d28344c005272d927b63302a6455b05c93
mysteriou <elio007@hotmail.fr>
2020-12-16 19:30:40 +0100

    ajout bouton afficher et cacher les parametre

--- public/script/carousel.js
+++ public/script/carousel.js
@@ -1,5 +1,4 @@
 
-
 function carousel(id,time){
  var myCarousel = document.querySelector(id)
 var carousel = new bootstrap.Carousel(myCarousel, {
@@ -7,4 +6,29 @@ var carousel = new bootstrap.Carousel(myCarousel, {
 })
 
 }
+
+ function display(){
+
+  var classname =  document.getElementById("parametre").className;
+
+  document.getElementById("btnparametre").innerText;
+
+  if(classname == "d-block"){
+
+  document.getElementById("btnparametre").innerText = "+";
+
+  document.getElementById("parametre").className ="d-none";
+
+  }else{
+
+  document.getElementById("btnparametre").innerText = "-";
+
+
+ document.getElementById("parametre").className ="d-block";
+
+
+  }
+
+ }
+
 
